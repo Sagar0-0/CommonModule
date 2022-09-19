30c015efbd9d6752cc4ce32cbf972b8857464cb0
Naman <namangr8y@gmail.com>
2022-09-19 18:57:53 +0530

    Added jetpack compose dependencies in the gradle

--- mobile/src/main/java/fit/asta/health/ui/theme/Type.kt
+++ mobile/src/main/java/fit/asta/health/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package fit.asta.health.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import fit.asta.health.R
 
-val Typography = Typography(
+val Typography3 = Typography(
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    )
+)
+
+val Heading1 = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.inter_light)),
+        fontWeight = FontWeight.Light,
+        fontSize =  96.sp
     )
 )
