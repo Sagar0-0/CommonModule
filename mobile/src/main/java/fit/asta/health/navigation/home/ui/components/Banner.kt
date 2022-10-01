9ffb5bd75ddb51dd9f6b2493d316f330aa1bf976
Sriram <sriramkalyan.p@gmail.com>
2022-10-01 20:32:52 +0530

    Home Screen refactoring

--- mobile/src/main/java/fit/asta/health/navigation/home/ui/components/Banner.kt
+++ mobile/src/main/java/fit/asta/health/navigation/home/ui/components/Banner.kt
@@ -1,82 +1,32 @@
-package fit.asta.health.home.view
+package fit.asta.health.navigation.home.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.util.lerp
-import com.google.accompanist.pager.*
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.PagerState
 import fit.asta.health.R
-import fit.asta.health.home.model.HomeScreenImageSliderData
-import fit.asta.health.home.model.sliderDataList
-import kotlinx.coroutines.delay
-import kotlin.math.absoluteValue
+import fit.asta.health.navigation.home.model.dummy.HomeScreenImageSliderData
 
-
-@Preview
-@ExperimentalPagerApi
-@Composable
-fun AutoSliding() {
-    val pagerState = rememberPagerState(
-        pageCount = sliderDataList.size
-    )
-
-    LaunchedEffect(key1 = pagerState.currentPage) {
-        delay(2500)
-        var newPosition = pagerState.currentPage + 1
-        if (newPosition > (sliderDataList.size - 1)) newPosition = 0
-        pagerState.animateScrollToPage(newPosition)
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        HorizontalPager(
-            state = pagerState,
-            verticalAlignment = Alignment.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) { page ->
-            Box(
-                modifier = Modifier
-                    .graphicsLayer {
-                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
-                        lerp(
-                            start = 0.85f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        ).also { scale ->
-                            scaleX = scale
-                            scaleY = scale
-                        }
-                    }
-                    .fillMaxWidth()
-                    .height(236.dp)
-            ) {
-                val sliderDataPages = sliderDataList[page]
-                Banner(page, sliderDataPages, pagerState)
-            }
-        }
-    }
-}
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-private fun Banner(
+fun Banner(
     page: Int,
     sliderDataPages: HomeScreenImageSliderData,
     pagerState: PagerState,
--- mobile/src/main/java/fit/asta/health/ui/layout/RememberWindowInfo.kt
+++ mobile/src/main/java/fit/asta/health/ui/layout/RememberWindowInfo.kt
@@ -1,4 +1,4 @@
-package fit.asta.health.home
+package fit.asta.health.ui.layout
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
