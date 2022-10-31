f84db1e12bb963e18f82fdee22f708920bcfca41
Sushiksha Naik <sushiksha.naik@gmail.com>
2022-10-31 11:11:21 +0530

    Water Water tools screen

---
+++ mobile/src/main/java/fit/asta/health/tools/water/WaterToolUI.kt
@@ -0,0 +1,55 @@
+package fit.asta.health.tools.water
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fit.asta.health.navigation.home.view.component.ToolList
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CardProgress()
+        }
+    }
+}
+
+@Composable
+fun CardProgress(){
+    Card(
+       // elevation = 8.dp,
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(16.dp))
+    { Column(
+        modifier = Modifier
+            .padding(16.dp)
+    ) {
+        CircularProgressIndicator(progress = 0.75f)
+        
+    }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface(Modifier.fillMaxSize()) {
+        CardProgress()
+    }
+
+}
