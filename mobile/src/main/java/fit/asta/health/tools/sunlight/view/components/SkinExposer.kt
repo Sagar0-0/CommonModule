07fc4ba43545da631acdcbcd67e1c74e51a3f4a5
Naman <namangr8y@gmail.com>
2022-11-04 20:38:32 +0530

    Walking Tool Done. Few Screens Left

--- mobile/src/main/java/fit/asta/health/tools/sunlight/view/components/SkinExposer.kt
+++ mobile/src/main/java/fit/asta/health/tools/sunlight/view/components/SkinExposer.kt
@@ -1,17 +1,18 @@
 package fit.asta.health.tools.sunlight.view.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -19,50 +20,147 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fit.asta.health.R
+import kotlin.reflect.KFunction1
 
 data class SkinExposerData(
+    val id: Int,
     val percentValue: String,
     val cardImg: Int,
+    var isSelected: Boolean = false,
 )
 
 @Preview
 @Composable
 fun SkinExposureLayout() {
 
-    val cardList = listOf(
-        SkinExposerData(percentValue = "40%", cardImg = R.drawable.girl_avatar_40),
-        SkinExposerData(percentValue = "30%", cardImg = R.drawable.boy_avatar_30),
-        SkinExposerData(percentValue = "45%", cardImg = R.drawable.boy_avatar_30),
-        SkinExposerData(percentValue = "50%", cardImg = R.drawable.boy_avatar_30),
-        SkinExposerData(percentValue = "25%", cardImg = R.drawable.boy_avatar_30),
-        SkinExposerData(percentValue = "40%", cardImg = R.drawable.boy_avatar_30),
-        SkinExposerData(percentValue = "20%", cardImg = R.drawable.boy_avatar_30),
-        SkinExposerData(percentValue = "20%", cardImg = R.drawable.boy_avatar_30),
-    )
+    val cardList = remember {
+        mutableStateListOf(
+            SkinExposerData(1, percentValue = "40%", cardImg = R.drawable.girl_avatar_40),
+            SkinExposerData(2, percentValue = "30%", cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(3, percentValue = "45%", cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(4, percentValue = "50%", cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(5, percentValue = "25%", cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(6, percentValue = "40%", cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(7, percentValue = "20%", cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(8, percentValue = "20%", cardImg = R.drawable.boy_avatar_30),
+        )
+    }
+
+
+    SkinExposureList(list = cardList,
+        rowTitle = "Please select skin exposure percentage based on clothing")
 
 }
 
-
-@Preview
 @Composable
-fun SkinExposureCard() {
-    Card(modifier = Modifier
-        .blur(radius = 5.dp)
-        .padding(8.dp),
-        shape = RoundedCornerShape(8.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(0x66959393))) {
+fun SkinExposureCardComponent(
+    itemData: SkinExposerData,
+    demoSelected: Boolean,
+    cardValue: String,
+    cardImg: Int,
+) {
+    Row(horizontalArrangement = Arrangement.Center) {
         Column(verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(painter = painterResource(id = R.drawable.boy_avatar_30),
-                contentDescription = null)
-            Text(text = "40%", fontSize = 18.sp, fontWeight = FontWeight.Bold, lineHeight = 25.2.sp)
+            Image(painter = painterResource(id = cardImg),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp))
+            Text(text = cardValue,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 25.2.sp)
+        }
+        if (demoSelected) {
+            Box(contentAlignment = Alignment.TopEnd) {
+                androidx.compose.material.Icon(imageVector = Icons.Default.Check,
+                    contentDescription = "Selected",
+                    tint = Color.Green,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .padding(top = 8.dp, end = 8.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun SkinExposureCard(
+    itemData: SkinExposerData,
+    onCheckChanged: KFunction1<SkinExposerData, Unit>,
+    cardValue: String,
+    cardImg: Int,
+) {
+
+    var demoSelected by remember {
+        mutableStateOf(false)
+    }
+
+    Card(modifier = Modifier
+        .clickable {
+            if (!itemData.isSelected) {
+                demoSelected = !demoSelected
+            }
+        }
+        .padding(8.dp), shape = RoundedCornerShape(8.dp), backgroundColor = Color(0x66959393)) {
+
+        SkinExposureCardComponent(cardValue = cardValue,
+            cardImg = cardImg,
+            itemData = itemData,
+            demoSelected = demoSelected)
+
+    }
+}
+
+class SkinExposerDataState(val list: MutableList<SkinExposerData>) {
+
+    // were updating the entire list in a single pass using its iterator
+    fun onItemSelected(selectedItemData: SkinExposerData) {
+        val iterator = list.listIterator()
+
+        while (iterator.hasNext()) {
+            val listItem = iterator.next()
+
+            iterator.set(if (listItem.id == selectedItemData.id) {
+                selectedItemData
+            } else {
+                listItem.copy(isSelected = false)
+            })
+        }
+    }
+}
+
+@Composable
+fun SkinExposureList(
+    list: MutableList<SkinExposerData>,
+    rowTitle: String,
+) {
+
+    val itemDataState = remember { SkinExposerDataState(list) }
+
+
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)) {
+            androidx.compose.material.Text(text = rowTitle,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Black)
         }
 
-        Box(contentAlignment = Alignment.TopStart) {
-            androidx.compose.material.Icon(imageVector = Icons.Default.Check,
-                contentDescription = "Selected",
-                tint = Color.Green,
-                modifier = Modifier.size(20.dp))
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyVerticalGrid(columns = GridCells.Fixed(2),
+            modifier = Modifier.padding(10.dp),
+            userScrollEnabled = false) {
+            items(itemDataState.list.size) {
+                SkinExposureCard(cardValue = itemDataState.list[it].percentValue,
+                    cardImg = itemDataState.list[it].cardImg,
+                    itemData = itemDataState.list[it],
+                    onCheckChanged = itemDataState::onItemSelected)
+            }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
---
+++ mobile/src/main/java/fit/asta/health/tools/walking/view/component/ModeSelectionLayout.kt
@@ -0,0 +1,22 @@
+package fit.asta.health.tools.walking.view.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import fit.asta.health.R
+import fit.asta.health.tools.sunlight.view.components.SkinExposerData
+import fit.asta.health.tools.sunlight.view.components.SkinExposureList
+
+@Composable
+fun ModeSelectionLayout() {
+
+    val cardList = remember {
+        mutableStateListOf(SkinExposerData(1,
+            percentValue = "Outdoor",
+            cardImg = R.drawable.boy_avatar_30),
+            SkinExposerData(id = 2, percentValue = "Indoor", cardImg = R.drawable.boy_avatar_30))
+    }
+
+    SkinExposureList(list = cardList, rowTitle = "Select mode of exercise")
+
+}
