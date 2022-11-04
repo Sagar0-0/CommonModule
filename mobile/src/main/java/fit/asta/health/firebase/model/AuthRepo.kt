3af37080de1dd090f3ca08cede9d48728893fdc4
Sriram <sriramkalyan.p@gmail.com>
2022-11-04 19:10:52 +0530

    Authentication in-progress

--- mobile/src/main/java/fit/asta/health/firebase/model/AuthRepo.kt
+++ mobile/src/main/java/fit/asta/health/firebase/model/AuthRepo.kt
@@ -1,7 +1,6 @@
 package fit.asta.health.firebase.model
 
 import android.app.Activity
-import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.AuthCredential
 import fit.asta.health.firebase.model.domain.User
 import fit.asta.health.firebase.model.domain.UserCred
@@ -11,9 +10,10 @@ import kotlinx.coroutines.flow.Flow
 interface AuthRepo {
     fun isAuthenticated(): Boolean
     fun getUser(): User?
-    fun firebaseSignInWithGoogle(googleAuthCredential: AuthCredential): MutableLiveData<User>?
+    fun firebaseSignInWithGoogle(googleAuthCredential: AuthCredential): Flow<ResultState<User>>
     fun createUserWithPhone(phone: String, activity: Activity): Flow<ResultState<String>>
     fun signWithCredential(otp: String): Flow<ResultState<String>>
-    fun createUser(auth: UserCred): Flow<ResultState<String>>
-    fun loginUser(auth: UserCred): Flow<ResultState<String>>
+    fun createUser(auth: UserCred): Flow<ResultState<User>>
+    fun loginUser(auth: UserCred): Flow<ResultState<User>>
+    fun logout()
 }
