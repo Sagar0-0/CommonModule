4e2d09d473dad41d77447d67dd4e3a20030d866f
Sriram <sriramkalyan.p@gmail.com>
2022-11-06 00:57:03 +0530

    Testimonials Integration in-progress

--- mobile/src/main/java/fit/asta/health/navigation/home/view/component/MyToolsAndViewAll.kt
+++ mobile/src/main/java/fit/asta/health/navigation/home/view/component/MyToolsAndViewAll.kt
@@ -2,25 +2,34 @@ package fit.asta.health.navigation.home.view.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyToolsAndViewAll(
-    myTools:String,
-    allTools:String
+    myTools: String,
+    allTools: String,
+    onClick: (value: Int) -> Unit,
 ) {
-    Row(horizontalArrangement = Arrangement.SpaceBetween,
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 24.dp)) {
+            .padding(vertical = 24.dp)
+    ) {
         Text(text = myTools, style = MaterialTheme.typography.h6, color = Color.Black)
         Box(modifier = Modifier.clickable(enabled = true, onClick = ({}))) {
-            Text(text = allTools, style = MaterialTheme.typography.h6, color = Color.Blue)
+            ClickableText(
+                text = AnnotatedString(allTools),
+                style = MaterialTheme.typography.h6,
+                onClick = onClick
+            )
         }
     }
 }
