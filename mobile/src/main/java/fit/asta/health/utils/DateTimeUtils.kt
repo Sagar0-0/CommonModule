5c27f1c3795b44458f2f31f265e21f0d3ca6957f
Sriram <sriramkalyan.p@gmail.com>
2022-11-27 22:22:44 +0530

    DateTime crash fix

--- mobile/src/main/java/fit/asta/health/utils/DateTimeUtils.kt
+++ mobile/src/main/java/fit/asta/health/utils/DateTimeUtils.kt
@@ -3,15 +3,23 @@ package fit.asta.health.utils
 import android.os.Build
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 
 //"2022-10-03%2012%20pm"
-fun getCurrentTime(format: String = "yyyy-MM-dd G 'at' HH:mm:ss z"): String {
+fun getCurrentTime(format: String = "yyyy-MM-dd HH:mm:ss"): String {
 
-    val sdf = SimpleDateFormat(format, Locale.getDefault())
-    return sdf.format(Date())
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        val ldt: LocalDateTime = LocalDateTime.now()
+        ldt.format(DateTimeFormatter.ofPattern(format))
+    } else {
+
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        sdf.format(Date())
+    }
 }
 
 fun getCurrentDate(format: String = "yyyy-MM-dd"): String {
@@ -23,7 +31,7 @@ fun getCurrentDate(format: String = "yyyy-MM-dd"): String {
     } else {
 
         val sdf = SimpleDateFormat(format, Locale.getDefault())
-        return sdf.format(Date())
+        sdf.format(Date())
     }
 }
 
