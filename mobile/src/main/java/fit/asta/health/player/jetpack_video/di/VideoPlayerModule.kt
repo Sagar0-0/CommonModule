fbeaa2793fb5a4dff6abd7636bbbe8355b580cc0
subhash <subhashkudi59@gmail.com>
2023-06-11 10:53:50 +0530

    video player

--- mobile/src/main/java/fit/asta/health/player/jetpack_video/di/VideoPlayerModule.kt
+++ mobile/src/main/java/fit/asta/health/player/jetpack_video/di/VideoPlayerModule.kt
@@ -1,6 +1,8 @@
 package fit.asta.health.player.jetpack_video.di
 
 import android.content.Context
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
 import androidx.media3.common.Player
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
@@ -20,10 +22,17 @@ object VideoPlayerModule {
     @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
     @ViewModelScoped
     fun  provideVideoPlayer(@ApplicationContext context: Context): Player {
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
         return ExoPlayer.Builder(context)
             .setMediaSourceFactory(
                 ProgressiveMediaSource.Factory(DefaultDataSource.Factory(context))
             )
+            .setAudioAttributes(audioAttributes, true)
+            .setHandleAudioBecomingNoisy(true)
+            .setWakeMode(C.WAKE_MODE_LOCAL)
             .build().apply {
                 playWhenReady = true
             }
