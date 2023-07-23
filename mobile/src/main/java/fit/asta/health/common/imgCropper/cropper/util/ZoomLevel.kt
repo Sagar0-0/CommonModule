1f37884a282727a5dc5d870b5788074ff388f70a
Sriram <sriramkalyan.p@gmail.com>
2023-07-23 11:40:19 +0530

    Moved image cropper to common

---
+++ mobile/src/main/java/fit/asta/health/common/imgCropper/cropper/util/ZoomLevel.kt
@@ -0,0 +1,8 @@
+package fit.asta.health.common.imgCropper.cropper.util
+
+/**
+ * Enum class for zoom levels
+ */
+enum class ZoomLevel {
+    Min, Mid, Max
+}
--- mobile/src/main/java/fit/asta/health/imageCropperV2/cropper/util/ZoomLevel.kt
+++
@@ -1,8 +0,0 @@
-package fit.asta.health.imageCropperV2.cropper.util
-
-/**
- * Enum class for zoom levels
- */
-enum class ZoomLevel {
-    Min, Mid, Max
-}
