882f8cb1a64629ddd4d33c4433de3275862c26f5
Subhash <subhashkudi59@gmail.com>
2023-08-05 19:00:51 +0530

    ref:change delete and skip functionality

--- mobile/src/main/java/fit/asta/health/scheduler/compose/screen/homescreen/HomeEvent.kt
+++ mobile/src/main/java/fit/asta/health/scheduler/compose/screen/homescreen/HomeEvent.kt
@@ -1,13 +1,12 @@
 package fit.asta.health.scheduler.compose.screen.homescreen
 
-import android.content.Context
 import fit.asta.health.scheduler.model.db.entity.AlarmEntity
 
 sealed class HomeEvent {
-    data class DeleteAlarm(val alarm: AlarmEntity, val context: Context,val event: Event) : HomeEvent()
-    data class UndoAlarm(val alarm: AlarmEntity, val context: Context,val event: Event) : HomeEvent()
-    data class SkipAlarm(val alarm: AlarmEntity, val context: Context,val event: Event) : HomeEvent()
-    data class UndoSkipAlarm(val alarm: AlarmEntity, val context: Context,val event: Event) : HomeEvent()
+    data class DeleteAlarm(val alarm: AlarmEntity) : HomeEvent()
+    data class RemoveAlarm(val alarm: AlarmEntity, val event: Event) : HomeEvent()
+    data class UndoAlarm(val alarm: AlarmEntity, val event: Event) : HomeEvent()
+    data class SkipAlarm(val alarm: AlarmEntity) : HomeEvent()
     data class EditAlarm(val alarm: AlarmEntity) : HomeEvent()
     object SetAlarm : HomeEvent()
 }
