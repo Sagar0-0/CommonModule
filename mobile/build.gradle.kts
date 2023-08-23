da54971fb9338a13f5ada973b85c0f7a115c5c80
Sriram <sriramkalyan.p@gmail.com>
2023-08-23 21:53:30 +0530

    fix: multi module resource issue

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -88,7 +88,7 @@ android {
 
     packaging {
         resources {
-            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+            excludes.add("/META-INF/{AL2.0,LGPL2.1,LICENSE.md,LICENSE-notice.md}")
         }
     }
 
@@ -129,7 +129,6 @@ dependencies {
     implementation(project(":feature:onboarding"))
     implementation(project(":feature:address"))
     implementation(project(":feature:feedback"))
-
 
     // Spotify App remote Dependency
     implementation(fileTree(mapOf("include" to listOf("*.jar", "*.aar"), "dir" to "libs")))
