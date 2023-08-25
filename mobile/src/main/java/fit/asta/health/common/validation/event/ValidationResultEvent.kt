e768b92c05d3cf160164144cb1c4d6cc5a4e6c57
Sriram <sriramkalyan.p@gmail.com>
2023-08-25 20:46:24 +0530

    refactor: common folder cleanup from mobile

--- mobile/src/main/java/fit/asta/health/common/validation/event/ValidationResultEvent.kt
+++
@@ -1,9 +0,0 @@
-package fit.asta.health.common.validation.event
-
-import fit.asta.health.common.utils.UiString
-
-sealed class ValidationResultEvent {
-
-    object Success : ValidationResultEvent()
-    data class Error(val message: UiString) : ValidationResultEvent()
-}
--- mobile/src/main/java/fit/asta/health/common/validation/util/TextFieldType.kt
+++
@@ -1,5 +0,0 @@
-package fit.asta.health.common.validation.util
-
-enum class TextFieldType {
-    Email, Phone, Password, Text
-}
