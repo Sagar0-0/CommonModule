67c604bbd9c7806846f9e32fffe8841a05038023
Anirban Basak <jeetbasak2002@gmail.com>
2023-08-30 03:52:09 +0530

    feat,refactor : Added more tests and some minor refactoring in previous tests

--- data/spotify/src/test/java/fit/asta/health/data/spotify/remote/SpotifyApiServiceTest.kt
+++ data/spotify/src/test/java/fit/asta/health/data/spotify/remote/SpotifyApiServiceTest.kt
@@ -5,7 +5,9 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import fit.asta.health.common.utils.ResponseState
 import fit.asta.health.common.utils.getResponseState
+import fit.asta.health.data.spotify.model.library.albums.SpotifyLibraryAlbumModel
 import fit.asta.health.data.spotify.model.library.following.SpotifyUserFollowingArtist
+import fit.asta.health.data.spotify.model.library.playlist.SpotifyUserPlaylistsModel
 import fit.asta.health.data.spotify.util.JsonReader
 import fit.asta.health.data.spotify.model.me.SpotifyMeModel
 import fit.asta.health.data.spotify.model.search.ArtistList
@@ -20,6 +22,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.test.assertEquals
@@ -51,7 +54,8 @@ class SpotifyApiServiceTest {
 
 
     @Nested
-    @DisplayName("Get Current User Details Tests")
+    @DisplayName("Get Current User Details")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class GetCurrentUser {
 
         @Test
@@ -88,7 +92,8 @@ class SpotifyApiServiceTest {
             server.enqueue(mockResponse)
 
             // Response from the Server
-            val response = getResponseState { api.getCurrentUserDetails(mapOf()) }
+            val response: ResponseState<SpotifyMeModel> =
+                getResponseState { api.getCurrentUserDetails(mapOf()) }
             server.takeRequest()
 
             // Assertion to check if the response is correct or not
@@ -99,6 +104,7 @@ class SpotifyApiServiceTest {
 
     @Nested
     @DisplayName("Get Current User Followed Artists")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class GetCurrentUserFollowedArtists {
 
         @Test
@@ -134,7 +140,8 @@ class SpotifyApiServiceTest {
             server.enqueue(mockResponse)
 
             // Response from the Server
-            val response = getResponseState { api.getCurrentUserFollowedArtists(mapOf()) }
+            val response: ResponseState<SpotifyUserFollowingArtist> =
+                getResponseState { api.getCurrentUserFollowedArtists(mapOf()) }
             server.takeRequest()
 
             // Assertion to check if the response is correct or not
@@ -145,6 +152,7 @@ class SpotifyApiServiceTest {
 
     @Nested
     @DisplayName("Get Current User Top Tracks")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class GetCurrentUserTopTracks {
 
         @Test
@@ -180,7 +188,8 @@ class SpotifyApiServiceTest {
             server.enqueue(mockResponse)
 
             // Response from the Server
-            val response = getResponseState { api.getCurrentUserTopTracks(mapOf()) }
+            val response: ResponseState<TrackList> =
+                getResponseState { api.getCurrentUserTopTracks(mapOf()) }
             server.takeRequest()
 
             // Assertion to check if the response is correct or not
@@ -191,6 +200,7 @@ class SpotifyApiServiceTest {
 
     @Nested
     @DisplayName("Get Current User Top Artists")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class GetCurrentUserTopArtists {
 
         @Test
@@ -226,7 +236,104 @@ class SpotifyApiServiceTest {
             server.enqueue(mockResponse)
 
             // Response from the Server
-            val response = getResponseState { api.getCurrentUserTopArtists(mapOf()) }
+            val response: ResponseState<ArtistList> =
+                getResponseState { api.getCurrentUserTopArtists(mapOf()) }
+            server.takeRequest()
+
+            // Assertion to check if the response is correct or not
+            assert(response is ResponseState.Error)
+        }
+    }
+
+
+    @Nested
+    @DisplayName("Get Current User Playlists")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class GetCurrentUserPlaylists {
+
+        @Test
+        fun `getCurrentUserPlaylists, returns Success`() = runTest {
+
+            // Mock Response
+            val mockResponse = MockResponse()
+
+            // Reading JSON File from the resources folder
+            val json = JsonReader.readJsonFile("/json/getCurrentUserPlaylists.json")
+            val localJsonData = gson.fromJson(json, SpotifyUserPlaylistsModel::class.java)
+            mockResponse.setBody(json)
+            server.enqueue(mockResponse)
+
+            // Response from the Server
+            val response = api.getCurrentUserPlaylists(mapOf())
+            server.takeRequest()
+
+            // Assertion to check if the response is correct or not
+            assertEquals(response, localJsonData)
+        }
+
+        @Test
+        fun `getCurrentUserPlaylists, returns Failed`() = runTest {
+
+            // Defining all the static functions return
+            mockkStatic(Log::class)
+            every { Log.e(any(), any()) } returns 0
+
+            // Mock Response
+            val mockResponse = MockResponse()
+            mockResponse.setResponseCode(404)
+            server.enqueue(mockResponse)
+
+            // Response from the Server
+            val response: ResponseState<SpotifyUserPlaylistsModel> =
+                getResponseState { api.getCurrentUserPlaylists(mapOf()) }
+            server.takeRequest()
+
+            // Assertion to check if the response is correct or not
+            assert(response is ResponseState.Error)
+        }
+    }
+
+
+    @Nested
+    @DisplayName("Get Current User Albums")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class GetCurrentUserAlbums {
+
+        @Test
+        fun `getCurrentUserAlbums, returns Success`() = runTest {
+
+            // Mock Response
+            val mockResponse = MockResponse()
+
+            // Reading JSON File from the resources folder
+            val json = JsonReader.readJsonFile("/json/getCurrentUserAlbums.json")
+            val localJsonData = gson.fromJson(json, SpotifyLibraryAlbumModel::class.java)
+            mockResponse.setBody(json)
+            server.enqueue(mockResponse)
+
+            // Response from the Server
+            val response = api.getCurrentUserAlbums(mapOf())
+            server.takeRequest()
+
+            // Assertion to check if the response is correct or not
+            assertEquals(response, localJsonData)
+        }
+
+        @Test
+        fun `getCurrentUserAlbums, returns Failed`() = runTest {
+
+            // Defining all the static functions return
+            mockkStatic(Log::class)
+            every { Log.e(any(), any()) } returns 0
+
+            // Mock Response
+            val mockResponse = MockResponse()
+            mockResponse.setResponseCode(404)
+            server.enqueue(mockResponse)
+
+            // Response from the Server
+            val response: ResponseState<SpotifyLibraryAlbumModel> =
+                getResponseState { api.getCurrentUserAlbums(mapOf()) }
             server.takeRequest()
 
             // Assertion to check if the response is correct or not
