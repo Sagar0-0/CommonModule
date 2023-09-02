58bf5cc8973d036f2d5f89617daa544b30258fcd
Anirban Basak <jeetbasak2002@gmail.com>
2023-09-02 19:01:12 +0530

    update : Added the test cases for the room db in spotify feature (there are bugs)

---
+++ data/spotify/src/test/java/fit/asta/health/data/spotify/local/MusicDaoTest.kt
@@ -0,0 +1,52 @@
+package fit.asta.health.data.spotify.local
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import fit.asta.health.data.spotify.model.common.Track
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.IOException
+import kotlin.jvm.Throws
+import kotlin.test.assertEquals
+
+class MusicDaoTest {
+
+    private lateinit var musicDao: MusicDao
+    private lateinit var database: MusicDatabase
+
+    @BeforeEach
+    fun beforeEach() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        database = Room.inMemoryDatabaseBuilder(context, MusicDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        musicDao = database.musicDao()
+    }
+
+    @AfterEach
+    @Throws(IOException::class)
+    fun afterEach() {
+        database.close()
+    }
+
+    @Test
+    fun `getAllTracks, returns success`() = runTest {
+
+        val track1 = mockk<Track>()
+        val track2 = mockk<Track>()
+        val mockTrack = listOf(track1, track2)
+
+        musicDao.insertTrack(track1)
+        musicDao.insertTrack(track2)
+
+        val response = musicDao.getAllTracks()
+
+        assertEquals(response.size, mockTrack.size)
+    }
+
+}
