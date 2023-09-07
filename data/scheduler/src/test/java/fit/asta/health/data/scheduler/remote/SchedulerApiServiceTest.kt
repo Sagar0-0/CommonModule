7326082b2f919108f7b009c2eaeb45682b591ffe
Subhash <subhashkudi59@gmail.com>
2023-09-08 02:02:33 +0530

    test:data:scheduler SchedulerApiServiceTest,AlarmBackendRepoImpTest

---
+++ data/scheduler/src/test/java/fit/asta/health/data/scheduler/remote/SchedulerApiServiceTest.kt
@@ -0,0 +1,179 @@
+package fit.asta.health.data.scheduler.remote
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import fit.asta.health.data.scheduler.remote.model.TodaySchedules
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerDeleteResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerGetListResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerGetResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerPutResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.Status
+import fit.asta.health.data.scheduler.remote.net.tag.AstaGetTagsListResponse
+import fit.asta.health.data.scheduler.remote.net.tag.NetCustomTag
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import okhttp3.MultipartBody
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import retrofit2.HttpException
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SchedulerApiServiceTest {
+    private lateinit var server: MockWebServer
+    private lateinit var api: SchedulerApiService
+
+    private val gson: Gson = GsonBuilder().create()
+
+    @BeforeEach
+    fun beforeEach() {
+        server = MockWebServer()
+        api = Retrofit.Builder()
+            .baseUrl(server.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(SchedulerApiService::class.java)
+    }
+
+    @AfterEach
+    fun afterEach() {
+        server.shutdown()
+    }
+
+    @Test
+    fun `getTodayDataFromBackend,return Success`() = runTest {
+        val dto = TodaySchedules()
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.getTodayDataFromBackend("", "", "", 0f, 0f)
+        server.takeRequest()
+
+        assertEquals(data, dto)
+    }
+
+    @Test
+    fun `getTodayDataFromBackend,return Error`() = runTest {
+        val res = MockResponse()
+        res.setResponseCode(404)
+        server.enqueue(res)
+        try {
+            api.getTodayDataFromBackend("", "", "", 0f, 0f)
+            server.takeRequest()
+        } catch (e: HttpException) {
+            assert(e.code() == 404)
+        }
+    }
+
+    @Test
+    fun `updateScheduleDataOnBackend,return Success`() = runTest {
+        val dto = AstaSchedulerPutResponse()
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.updateScheduleDataOnBackend(mockk())
+        server.takeRequest()
+
+        assertEquals(data.data.flag, dto.data.flag)
+    }
+
+    @Test
+    fun `getScheduleDataFromBackend,return Success`() = runTest {
+        val dto = AstaSchedulerGetResponse(
+            data = mockk(), status = Status(200, "")
+        )
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.getScheduleDataFromBackend("")
+        server.takeRequest()
+
+        assertEquals(data.status.code, dto.status.code)
+    }
+
+    @Test
+    fun `getScheduleListDataFromBackend,return Success`() = runTest {
+        val dto = AstaSchedulerGetListResponse(
+            data = listOf(), status = Status(200, "")
+        )
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.getScheduleListDataFromBackend("")
+        server.takeRequest()
+
+        assertEquals(data.status.code, dto.status.code)
+    }
+
+    @Test
+    fun `deleteScheduleDataFromBackend,return Success`() = runTest {
+        val dto = AstaSchedulerDeleteResponse(
+            data = mockk(), status = Status(200, "")
+        )
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.deleteScheduleDataFromBackend("")
+        server.takeRequest()
+
+        assertEquals(data.status.code, dto.status.code)
+    }
+
+    @Test
+    fun `getTagListFromBackend,return Success`() = runTest {
+        val dto = AstaGetTagsListResponse()
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.getTagListFromBackend("")
+        server.takeRequest()
+
+        assertEquals(data.status.code, dto.status.code)
+    }
+
+    @Test
+    fun `deleteTagFromBackend,return Success`() = runTest {
+        val dto = fit.asta.health.network.data.Status()
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+
+        val data = api.deleteTagFromBackend("", "")
+        server.takeRequest()
+
+        assertEquals(data.code, dto.code)
+    }
+
+    @Test
+    fun `updateScheduleTag,return Success`() = runTest {
+        val dto = fit.asta.health.network.data.Status()
+        val json = gson.toJson(dto)!!
+        val res = MockResponse()
+        res.setBody(json)
+        server.enqueue(res)
+        val file = MultipartBody.Builder()
+            .addFormDataPart("file", "file.txt")
+            .build()
+        val data = api.updateScheduleTag(NetCustomTag(), file.part(0))
+        val requestReceived = server.takeRequest()
+        assertEquals(requestReceived.method, "PUT")
+        assertEquals(requestReceived.path, "/tag/put")
+        assertEquals(data, dto)
+    }
+}
---
+++ data/scheduler/src/test/java/fit/asta/health/data/scheduler/repo/AlarmBackendRepoImpTest.kt
@@ -0,0 +1,214 @@
+package fit.asta.health.data.scheduler.repo
+
+import android.content.Context
+import fit.asta.health.common.utils.ResponseState
+import fit.asta.health.data.scheduler.db.entity.AlarmEntity
+import fit.asta.health.data.scheduler.remote.SchedulerApiService
+import fit.asta.health.data.scheduler.remote.model.TodaySchedules
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerDeleteResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerGetListResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerGetResponse
+import fit.asta.health.data.scheduler.remote.net.scheduler.AstaSchedulerPutResponse
+import fit.asta.health.data.scheduler.remote.net.tag.AstaGetTagsListResponse
+import fit.asta.health.data.scheduler.remote.net.tag.NetCustomTag
+import fit.asta.health.network.data.Status
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.spyk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AlarmBackendRepoImpTest {
+    private lateinit var repo: AlarmBackendRepoImp
+
+    @MockK(relaxed = true)
+    lateinit var remoteApi: SchedulerApiService
+
+    @MockK
+    lateinit var context: Context
+
+    @BeforeEach
+    fun beforeEach() {
+        MockKAnnotations.init(this, relaxed = true)
+        repo = spyk(
+            AlarmBackendRepoImp(
+                context = context, remoteApi
+            )
+        )
+    }
+
+    @AfterEach
+    fun afterEach() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `getTodayDataFromBackend,return Success`() = runTest {
+        coEvery {
+            remoteApi.getTodayDataFromBackend(
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        } returns TodaySchedules()
+        val res = repo.getTodayDataFromBackend("", "", "", 0f, 0f)
+        coVerify { remoteApi.getTodayDataFromBackend("", "", "", 0f, 0f) }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `getTodayDataFromBackend,return Error`() = runTest {
+        coEvery {
+            remoteApi.getTodayDataFromBackend(
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        } throws Exception()
+        val res = repo.getTodayDataFromBackend("", "", "", 0f, 0f)
+        coVerify { remoteApi.getTodayDataFromBackend("", "", "", 0f, 0f) }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `getScheduleDataFromBackend,return Success`() = runTest {
+        coEvery { remoteApi.getScheduleDataFromBackend(any()) } returns AstaSchedulerGetResponse(
+            mockk(), mockk()
+        )
+        val res = repo.getScheduleDataFromBackend("")
+        coVerify { remoteApi.getScheduleDataFromBackend("") }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `getScheduleDataFromBackend,return Error`() = runTest {
+        coEvery { remoteApi.getScheduleDataFromBackend(any()) } throws Exception()
+        val res = repo.getScheduleDataFromBackend("")
+        coVerify { remoteApi.getScheduleDataFromBackend("") }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `getScheduleListDataFromBackend,return Success`() = runTest {
+        coEvery { remoteApi.getScheduleListDataFromBackend(any()) } returns AstaSchedulerGetListResponse(
+            mockk(), mockk()
+        )
+        val res = repo.getScheduleListDataFromBackend("")
+        coVerify { remoteApi.getScheduleListDataFromBackend("") }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `getScheduleListDataFromBackend,return Error`() = runTest {
+        coEvery { remoteApi.getScheduleListDataFromBackend(any()) } throws Exception()
+        val res = repo.getScheduleListDataFromBackend("")
+        coVerify { remoteApi.getScheduleListDataFromBackend("") }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `getTagListFromBackend,return Success`() = runTest {
+        coEvery { remoteApi.getTagListFromBackend(any()) } returns AstaGetTagsListResponse(
+            mockk(), mockk()
+        )
+        val res = repo.getTagListFromBackend("")
+        coVerify { remoteApi.getTagListFromBackend("") }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `getTagListFromBackend,return Error`() = runTest {
+        coEvery { remoteApi.getTagListFromBackend(any()) } throws Exception()
+        val res = repo.getTagListFromBackend("")
+        coVerify { remoteApi.getTagListFromBackend("") }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `updateScheduleDataOnBackend,return Success`() = runTest {
+        coEvery { remoteApi.updateScheduleDataOnBackend(any()) } returns AstaSchedulerPutResponse()
+        val alarmEntity = mockk<AlarmEntity>()
+        val res = repo.updateScheduleDataOnBackend(alarmEntity)
+        coVerify { remoteApi.updateScheduleDataOnBackend(alarmEntity) }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `updateScheduleDataOnBackend,return Error`() = runTest {
+        coEvery { remoteApi.updateScheduleDataOnBackend(any()) } throws Exception()
+        val alarmEntity = mockk<AlarmEntity>()
+        val res = repo.updateScheduleDataOnBackend(alarmEntity)
+        coVerify { remoteApi.updateScheduleDataOnBackend(alarmEntity) }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `deleteScheduleDataFromBackend,return Success`() = runTest {
+        coEvery { remoteApi.deleteScheduleDataFromBackend(any()) } returns AstaSchedulerDeleteResponse(
+            mockk(), mockk()
+        )
+        val res = repo.deleteScheduleDataFromBackend("123")
+        coVerify { remoteApi.deleteScheduleDataFromBackend("123") }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `deleteScheduleDataFromBackend,return Error`() = runTest {
+        coEvery { remoteApi.deleteScheduleDataFromBackend(any()) } throws Exception()
+        val res = repo.deleteScheduleDataFromBackend("123")
+        coVerify { remoteApi.deleteScheduleDataFromBackend("123") }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `deleteTagFromBackend,return Success`() = runTest {
+        coEvery { remoteApi.deleteTagFromBackend(any(), any()) } returns Status()
+        val res = repo.deleteTagFromBackend("", "")
+        coVerify { remoteApi.deleteTagFromBackend("", "") }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `deleteTagFromBackend,return Error`() = runTest {
+        coEvery { remoteApi.deleteTagFromBackend(any(), any()) } throws Exception()
+        val res = repo.deleteTagFromBackend("", "")
+        coVerify { remoteApi.deleteTagFromBackend("", "") }
+        assert(res is ResponseState.Error)
+    }
+
+    @Test
+    fun `updateScheduleTag,return Success`() = runTest {
+        coEvery { remoteApi.updateScheduleTag(any(), any()) } returns Status()
+        val tag = NetCustomTag()
+        val uri = mockk<android.net.Uri>()
+        tag.localUrl = uri
+        tag.name = "name"
+        val res = repo.updateScheduleTag(tag)
+        coVerify { remoteApi.updateScheduleTag(tag, any()) }
+        assert(res is ResponseState.Success)
+    }
+
+    @Test
+    fun `updateScheduleTag,return Error`() = runTest {
+        coEvery { remoteApi.updateScheduleTag(any(), any()) } throws Exception()
+        val tag = NetCustomTag()
+        val uri = mockk<android.net.Uri>()
+        tag.localUrl = uri
+        tag.name = "name"
+        val res = repo.updateScheduleTag(tag)
+        coVerify { remoteApi.updateScheduleTag(tag, any()) }
+        assert(res is ResponseState.Error)
+    }
+
+
+}
