6e275de3c081e8a4ff18eab58acd9a810a1375c8
Anirban Basak <jeetbasak2002@gmail.com>
2023-09-23 09:22:12 +0530

    update : Made the Assist Chip

---
+++ core/designsystem/src/main/java/fit/asta/health/designsystemx/molecular/previews/AstaChipScreen.kt
@@ -0,0 +1,85 @@
+package fit.asta.health.designsystemx.molecular.previews
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.TrackChanges
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fit.asta.health.designsystemx.AstaThemeX
+import fit.asta.health.designsystemx.molecular.chip.AstaAssistChip
+import fit.asta.health.designsystemx.molecular.texts.TitleTexts
+
+
+// Preview Function
+@Preview(
+    "Light Button",
+    heightDp = 1100
+)
+@Preview(
+    name = "Dark Button",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    heightDp = 1100
+)
+@Composable
+private fun DefaultPreview1() {
+    AstaThemeX {
+        Surface {
+            AstaChipScreen()
+        }
+    }
+}
+
+@Composable
+fun AstaChipScreen() {
+    AstaThemeX {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(AstaThemeX.spacingX.medium)
+            ) {
+
+                // Filled Button Section
+                TitleTexts.Large(text = "Assist Chips")
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(AstaThemeX.spacingX.medium),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    AstaAssistChip(
+                        onClick = {},
+                        textToShow = "Assist Chip",
+                        leadingIcon = Icons.Default.Person,
+                        trailingIcon = Icons.Default.TrackChanges
+                    )
+
+                    AstaAssistChip(
+                        enabled = false,
+                        onClick = {},
+                        textToShow = "Assist Chip",
+                        leadingIcon = Icons.Default.Person,
+                        trailingIcon = Icons.Default.TrackChanges
+                    )
+                }
+            }
+        }
+    }
+}
