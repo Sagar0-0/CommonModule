e740bd986fa464e075f46793d68b9ec6bc0a233b
Subhash <subhashkudi59@gmail.com>
2023-09-25 18:19:21 +0530

    feat:mobile: mediation tool

---
+++ data/meditation/build.gradle.kts
@@ -0,0 +1,31 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.asta.android.library)
+    alias(libs.plugins.asta.android.hilt)
+    alias(libs.plugins.asta.android.test.data)
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "fit.asta.health.meditation"
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+dependencies {
+
+    implementation(project(":core:network"))
+    implementation(project(":core:common"))
+    implementation(project(":resources:strings"))
+    implementation(project(":resources:drawables"))
+
+    implementation(libs.gson)
+    implementation(libs.retrofit.core)
+    implementation(libs.converter.gson)
+    // Room Library
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+    androidTestImplementation(libs.androidx.room.testing)
+}
---
+++ feature/meditation/build.gradle.kts
@@ -0,0 +1,67 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.asta.android.library)
+    alias(libs.plugins.asta.android.hilt)
+    alias(libs.plugins.asta.android.library.compose)
+    alias(libs.plugins.asta.android.test.feature)
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "fit.asta.health.meditation"
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    //Jetpack Compose - Material theme components
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.window.size)
+    implementation(libs.androidx.material.icons.extended.android)
+    implementation(libs.compose.theme.adapter)
+
+    implementation(project(":libs:media"))
+    implementation(project(":data:meditation"))
+    implementation(project(":data:auth"))
+    implementation(project(":core:common"))
+    implementation(project(":core:test"))
+    implementation(project(":core:datastore"))
+    implementation(project(":core:network"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":resources:strings"))
+    implementation(project(":resources:drawables"))
+
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.material.icons.extended.android)
+
+    implementation(libs.coil.kt.compose)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.hilt.android)
+
+    kapt(libs.hilt.compiler)
+    kapt(libs.hilt.ext.compiler)
+//    implementation(libs.androidx.hilt.common)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.gson)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.common)
+    implementation(libs.androidx.media3.session)
+    //Image - Glide library
+    implementation(libs.glide)
+
+    implementation(libs.androidx.lifecycle.extensions)
+}
