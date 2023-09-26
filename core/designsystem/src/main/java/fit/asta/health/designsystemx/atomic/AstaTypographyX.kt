e350189cd010acf6455fab22f5ba30b8d1e405f9
Anirban Basak <jeetbasak2002@gmail.com>
2023-09-26 18:03:59 +0530

    update : New Updated typography added

--- core/designsystem/src/main/java/fit/asta/health/designsystemx/atomic/AstaTypographyX.kt
+++ core/designsystem/src/main/java/fit/asta/health/designsystemx/atomic/AstaTypographyX.kt
@@ -15,73 +15,110 @@ import fit.asta.health.core.designsystem.R
 internal val AstaTypographyX = Typography(
 
     displayLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_light)),
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
         fontWeight = FontWeight.Light,
-        fontSize = 96.sp
-    ), displayMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_light)),
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = 0.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
         fontWeight = FontWeight.Light,
-        fontSize = 60.sp
-    ), displaySmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
         fontWeight = FontWeight.Normal,
-        fontSize = 48.sp
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp
     ),
 
     headlineLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
-        fontWeight = FontWeight.Normal,
-        fontSize = 34.sp
-    ), headlineMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
-        fontWeight = FontWeight.Normal,
-        fontSize = 24.sp
-    ), headlineSmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
-        fontWeight = FontWeight.Normal,
-        fontSize = 20.sp
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+        lineHeight = 30.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 25.2.sp,
+        letterSpacing = 0.sp
     ),
 
     titleLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ), titleMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_medium)),
-        fontWeight = FontWeight.Medium,
-        fontSize = 16.sp
-    ), titleSmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_medium)),
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 25.2.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 22.4.sp,
+        letterSpacing = 0.1.sp
     ),
 
     bodyLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ), bodyMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
         fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ), bodySmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
     ),
 
     labelLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_medium)),
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp
-    ), labelMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
         fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ), labelSmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.inter_regular)),
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+//        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+//        fontWeight = FontWeight.SemiBold,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     )
 )
 internal val LocalTypographyX = compositionLocalOf { AstaTypographyX }
