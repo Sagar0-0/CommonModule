52e05271b64c83b7c26ed73f0d4e81c212ebd349
Anirban Basak <jeetbasak2002@gmail.com>
2023-10-22 04:56:12 +0530

    refactor : Polish and refactored the Terms/Privacy UI part in Auth

---
+++ feature/auth/src/main/java/fit/asta/health/feature/auth/components/TermAndPrivacyUI.kt
@@ -0,0 +1,144 @@
+package fit.asta.health.feature.auth.components
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import fit.asta.health.common.utils.getImgUrl
+import fit.asta.health.designsystem.AppTheme
+import fit.asta.health.resources.strings.R
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+/**
+ * This composable function creates the Annotated String in the UI
+ *
+ * @param modifier This is modifications which may be passed from the Parent Composable
+ * @param onTermsClick This function is called when the User clicks on the Terms Text
+ * @param onPrivacyClick This function is called when the User clicks on the Privacy Text
+ */
+@Composable
+fun TermAndPrivacyUI(
+    modifier: Modifier = Modifier,
+    onTermsClick: (String) -> Unit,
+    onPrivacyClick: (String) -> Unit
+) {
+
+    val context = LocalContext.current
+
+    // Building the Annotated string
+    val annotatedLinkString: AnnotatedString = buildAnnotatedString {
+        val str = stringResource(id = R.string.tnc_text)
+        val termStartIndex = str.indexOf("Terms")
+        val termEndIndex = termStartIndex + 16
+        val privacyStartIndex = str.indexOf("Privacy")
+        val privacyEndIndex = privacyStartIndex + 14
+        append(str)
+
+        // String Before Terms of Service Annotated Link
+        NormalText(startIndex = 0, endIndex = termStartIndex - 1)
+
+        // Terms of Service Annotated String
+        LinkedString(
+            textToAnnotate = "terms",
+            annotation = getImgUrl(context.getString(R.string.url_terms_of_use)),
+            startIndex = termStartIndex,
+            endIndex = termEndIndex
+        )
+
+        // String between both the Annotated String
+        NormalText(startIndex = termEndIndex + 1, endIndex = privacyStartIndex - 1)
+
+        // Privacy Policy Annotated String
+        LinkedString(
+            textToAnnotate = "privacy",
+            annotation = getImgUrl(context.getString(R.string.url_privacy_policy)),
+            startIndex = privacyStartIndex,
+            endIndex = privacyEndIndex
+        )
+    }
+
+    // The Whole Clickable Composable which shows the Text UI
+    ClickableText(
+        modifier = modifier,
+        text = annotatedLinkString
+    ) {
+        annotatedLinkString
+            .getStringAnnotations("terms", it, it)
+            .firstOrNull()?.let { stringAnnotation ->
+                onTermsClick(
+                    URLEncoder.encode(
+                        stringAnnotation.item,
+                        StandardCharsets.UTF_8.toString()
+                    )
+                )
+            }
+        annotatedLinkString
+            .getStringAnnotations("privacy", it, it)
+            .firstOrNull()?.let { stringAnnotation ->
+                onPrivacyClick(
+                    URLEncoder.encode(
+                        stringAnnotation.item,
+                        StandardCharsets.UTF_8.toString()
+                    )
+                )
+            }
+    }
+}
+
+/**
+ * This composable function is used to add Style to a Normal Text in the Terms and Condition UI
+ *
+ * @param startIndex This is the start Index from where we need to make this text
+ * @param endIndex This is the end Index until where we need to make this text
+ */
+@Composable
+private fun AnnotatedString.Builder.NormalText(startIndex: Int, endIndex: Int) = addStyle(
+    style = AppTheme.customTypography.body.level2
+        .toSpanStyle()
+        .copy(color = AppTheme.colors.onSurface),
+    start = startIndex,
+    end = endIndex
+)
+
+/**
+ * This composable function is used to add Style to the Annotated Linked String in the Terms and
+ * Conditions UI
+ *
+ * @param textToAnnotate This is the tag used to distinguish annotations
+ * @param annotation This is the string annotation that is attached
+ * @param startIndex This is start Index of the string
+ * @param endIndex This is the end index of the String
+ */
+@Composable
+private fun AnnotatedString.Builder.LinkedString(
+    textToAnnotate: String,
+    annotation: String,
+    startIndex: Int,
+    endIndex: Int
+) {
+
+    // This function adds the Style and decorations and typography for the Annotated String
+    addStyle(
+        style = AppTheme.customTypography.body.level2
+            .toSpanStyle()
+            .copy(
+                textDecoration = TextDecoration.Underline,
+                color = AppTheme.colors.primary
+            ),
+        start = startIndex,
+        end = endIndex
+    )
+
+    // This function sets an Annotations for the Strings
+    addStringAnnotation(
+        tag = textToAnnotate,
+        annotation = annotation,
+        start = startIndex,
+        end = endIndex
+    )
+}
--- feature/auth/src/main/java/fit/asta/health/feature/auth/screens/AuthScreen.kt
+++ feature/auth/src/main/java/fit/asta/health/feature/auth/screens/AuthScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,15 +15,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
 import fit.asta.health.common.utils.UiState
-import fit.asta.health.common.utils.getImgUrl
 import fit.asta.health.common.utils.toStringFromResId
 import fit.asta.health.data.onboarding.model.OnboardingData
 import fit.asta.health.designsystem.AppTheme
@@ -32,11 +24,10 @@ import fit.asta.health.designsystem.molecular.AppRetryCard
 import fit.asta.health.designsystem.molecular.animations.AppDivider
 import fit.asta.health.designsystem.molecular.animations.ShimmerAnimation
 import fit.asta.health.designsystem.molecular.texts.TitleTexts
+import fit.asta.health.feature.auth.components.TermAndPrivacyUI
 import fit.asta.health.feature.auth.util.GoogleSignIn
 import fit.asta.health.feature.auth.util.OnboardingDataPager
 import fit.asta.health.feature.auth.util.PhoneSignIn
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
 import fit.asta.health.resources.drawables.R as DrawR
 import fit.asta.health.resources.strings.R as StringR
 
@@ -46,7 +37,6 @@ internal fun AuthScreen(
     onboardingState: UiState<List<OnboardingData>>,
     onUiEvent: (AuthUiEvent) -> Unit
 ) {
-    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -62,7 +52,10 @@ internal fun AuthScreen(
             }
 
             UiState.Loading -> {
-                ShimmerAnimation(cardHeight = AppTheme.cardHeight.level3, cardWidth = AppTheme.cardHeight.level2)
+                ShimmerAnimation(
+                    cardHeight = AppTheme.cardHeight.level3,
+                    cardWidth = AppTheme.cardHeight.level2
+                )
             }
 
             else -> {}
@@ -84,7 +77,10 @@ internal fun AuthScreen(
             }
 
             is UiState.Loading -> {
-                ShimmerAnimation(cardHeight = AppTheme.cardHeight.level3, cardWidth = AppTheme.cardHeight.level2)
+                ShimmerAnimation(
+                    cardHeight = AppTheme.cardHeight.level3,
+                    cardWidth = AppTheme.cardHeight.level2
+                )
             }
 
             else -> {
@@ -120,79 +116,15 @@ internal fun AuthScreen(
             }
         }
 
-
-        val annotatedLinkString: AnnotatedString = buildAnnotatedString {
-
-            val str = stringResource(id = StringR.string.tnc_text)
-            val startTIndex = str.indexOf("Terms")
-            val endTIndex = startTIndex + 16
-            val startPIndex = str.indexOf("Privacy")
-            val endPIndex = startPIndex + 14
-            append(str)
-            addStyle(
-                style = SpanStyle(
-                    color = AppTheme.colors.onBackground
-                ), start = 0, end = startTIndex - 1
-            )
-            addStyle(
-                style = SpanStyle(
-                    color = AppTheme.colors.onBackground
-                ), start = endTIndex + 1, end = startPIndex - 1
-            )
-            addStyle(
-                style = SpanStyle(
-                    color = AppTheme.colors.primary,
-                    textDecoration = TextDecoration.Underline
-                ), start = startTIndex, end = endTIndex
-            )
-            addStyle(
-                style = SpanStyle(
-                    color = AppTheme.colors.primary,
-                    textDecoration = TextDecoration.Underline
-                ), start = startPIndex, end = endPIndex
-            )
-
-            addStringAnnotation(
-                tag = "terms",
-                annotation = getImgUrl(context.getString(StringR.string.url_terms_of_use)),
-                start = startTIndex,
-                end = endTIndex
-            )
-
-            addStringAnnotation(
-                tag = "privacy",
-                annotation = getImgUrl(context.getString(StringR.string.url_privacy_policy)),
-                start = startPIndex,
-                end = endPIndex
-            )
-
-        }
-
-        ClickableText(
+        TermAndPrivacyUI(
             modifier = Modifier
                 .padding(vertical = AppTheme.spacing.level2)
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            text = annotatedLinkString,
-            onClick = {
-                annotatedLinkString
-                    .getStringAnnotations("terms", it, it)
-                    .firstOrNull()?.let { stringAnnotation ->
-                        val url = URLEncoder.encode(
-                            stringAnnotation.item,
-                            StandardCharsets.UTF_8.toString()
-                        )
-                        onUiEvent(AuthUiEvent.NavigateToWebView(url))
-                    }
-                annotatedLinkString
-                    .getStringAnnotations("privacy", it, it)
-                    .firstOrNull()?.let { stringAnnotation ->
-                        val url = URLEncoder.encode(
-                            stringAnnotation.item,
-                            StandardCharsets.UTF_8.toString()
-                        )
-                        onUiEvent(AuthUiEvent.NavigateToWebView(url))
-                    }
+                .fillMaxWidth(),
+            onTermsClick = {
+                onUiEvent(AuthUiEvent.NavigateToWebView(url = it))
+            },
+            onPrivacyClick = {
+                onUiEvent(AuthUiEvent.NavigateToWebView(url = it))
             }
         )
     }
