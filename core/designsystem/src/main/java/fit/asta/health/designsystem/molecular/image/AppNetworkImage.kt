88503e786de7ecd576fa280240a501f3daf18115
Anirban Basak <jeetbasak2002@gmail.com>
2023-10-22 01:24:41 +0530

    feat : Added Shimmer Effect and refactored the images to have the shimmer Effect during Loading from Server

--- core/designsystem/src/main/java/fit/asta/health/designsystem/molecular/image/AppNetworkImage.kt
+++ core/designsystem/src/main/java/fit/asta/health/designsystem/molecular/image/AppNetworkImage.kt
@@ -1,14 +1,18 @@
 package fit.asta.health.designsystem.molecular.image
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import fit.asta.health.designsystem.AppTheme
+import fit.asta.health.designsystem.atomic.modifier.appShimmerAnimation
 
 
 /**
@@ -34,16 +38,40 @@ fun AppNetworkImage(
     contentDescription: String? = null,
     contentScale: ContentScale = ContentScale.FillBounds,
     colorFilter: ColorFilter? = null,
-    alpha: Float = AppTheme.alphaValues.level5,
+    alpha: Float = AppTheme.alphaValues.level5
 ) {
-    AsyncImage(
+
+    // This variable states if the painter is loading from the URL or is already loaded
+    val isLoading = remember { mutableStateOf(true) }
+
+    // This is the AsyncPainterImage
+    val painter = rememberAsyncImagePainter(
         model = model,
+        onError = {
+
+            // Loading state is turned back to false
+            isLoading.value = false
+        },
+        onSuccess = {
+
+            // Loading state is turned back to false
+            isLoading.value = false
+        },
+        onLoading = {
+
+            // Loading State is turned to true since the loading is started
+            isLoading.value = true
+        },
+        error = painterResource(id = AppConstImages.errorImg)
+    )
+
+    // This is the Fetched image which will be shown when the Image is fetched from the Server
+    Image(
+        painter = painter,
         contentDescription = contentDescription,
-        modifier = modifier,
+        modifier = modifier.appShimmerAnimation(isLoading.value),
         contentScale = contentScale,
-        placeholder = placeholder,
         alpha = alpha,
-        error = painterResource(id = AppConstImages.errorImg),
-        colorFilter = colorFilter
+        colorFilter = colorFilter,
     )
 }
