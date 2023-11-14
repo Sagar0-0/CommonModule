31d4de92959bcd040c1af3c96c53f65c81aa3043
Sagar <sagar.0dev@gmail.com>
2023-11-14 13:00:40 +0530

    discounts data module refactor

--- data/payments/discounts/src/main/java/fit/asta/health/discounts/repo/DiscountsRepoImpl.kt
+++ data/payments/discounts/src/main/java/fit/asta/health/discounts/repo/DiscountsRepoImpl.kt
@@ -1,14 +1,16 @@
 package fit.asta.health.discounts.repo
 
+import fit.asta.health.common.utils.IODispatcher
 import fit.asta.health.common.utils.getApiResponseState
 import fit.asta.health.discounts.remote.DiscountsApi
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class DiscountsRepoImpl(
+class DiscountsRepoImpl @Inject constructor(
     private val remoteApi: DiscountsApi,
-    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.Default
+    @IODispatcher private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : DiscountsRepo {
 
     override suspend fun getData() = withContext(coroutineDispatcher) {
