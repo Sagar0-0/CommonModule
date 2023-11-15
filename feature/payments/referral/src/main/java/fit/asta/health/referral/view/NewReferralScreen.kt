ebb29c4e28f72f3ba5ea6129978a002054729b34
Naman <namangr8y@gmail.com>
2023-11-15 12:44:34 +0530

    New Ref Screen...

--- feature/payments/referral/src/main/java/fit/asta/health/referral/view/NewReferralScreen.kt
+++ feature/payments/referral/src/main/java/fit/asta/health/referral/view/NewReferralScreen.kt
@@ -1,7 +1,6 @@
 package fit.asta.health.referral.view
 
 import android.content.res.Configuration
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,35 +10,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.Diamond
 import androidx.compose.material.icons.filled.GroupAdd
 import androidx.compose.material.icons.filled.Link
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fit.asta.health.designsystem.AppTheme
-import fit.asta.health.designsystem.molecular.animations.AppDivider
 import fit.asta.health.designsystem.molecular.background.AppSurface
 import fit.asta.health.designsystem.molecular.background.AppTopBar
 import fit.asta.health.designsystem.molecular.button.AppFilledButton
+import fit.asta.health.designsystem.molecular.button.AppIconButton
 import fit.asta.health.designsystem.molecular.button.AppTextButton
 import fit.asta.health.designsystem.molecular.cards.AppCard
+import fit.asta.health.designsystem.molecular.icon.AppIcon
 import fit.asta.health.designsystem.molecular.image.AppLocalImage
 import fit.asta.health.designsystem.molecular.texts.BodyTexts
 import fit.asta.health.designsystem.molecular.texts.CaptionTexts
 import fit.asta.health.designsystem.molecular.texts.HeadingTexts
 import fit.asta.health.designsystem.molecular.texts.LargeTexts
-import fit.asta.health.designsystem.molecular.texts.TitleTexts
 import fit.asta.health.resources.drawables.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -53,7 +57,7 @@ import fit.asta.health.resources.drawables.R
     heightDp = 1100
 )
 @Composable
-fun NewReferralDesign() {
+fun NewReferralDesign(shareRefLink: () -> Unit = {}, copyRefCode: () -> Unit = {}) {
 
     AppTheme {
         AppSurface(modifier = Modifier.fillMaxSize()) {
@@ -63,11 +67,14 @@ fun NewReferralDesign() {
                     .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                AppTopBar(title = "Refer and Earn", backIcon = Icons.Filled.ArrowBackIosNew)
+                AppTopBar(
+                    title = "Refer and Earn",
+                    backIcon = Icons.Filled.ArrowBackIosNew,
+                    onBack = {})
                 Spacer(modifier = Modifier.height(16.dp))
                 AppLocalImage(
                     painter = painterResource(id = R.drawable.ref_ed_2),
-                    modifier = Modifier.aspectRatio(AppTheme.aspectRatio.common),
+                    modifier = Modifier.aspectRatio(AppTheme.aspectRatio.wideScreen),
                     contentScale = ContentScale.Fit
                 )
                 Spacer(modifier = Modifier.height(16.dp))
@@ -76,20 +83,17 @@ fun NewReferralDesign() {
                 ) {
                     AppFilledButton(
                         textToShow = "Share your link",
-                        trailingIcon = Icons.Filled.Link
-                    ) {
-
-                    }
+                        trailingIcon = Icons.Filled.Link, onClick = shareRefLink
+                    )
                 }
                 LargeTexts.Level2(
                     text = "OR",
                     color = AppTheme.colors.onSurfaceVariant,
                 )
-                TitleTexts.Level4(text = "Share with your Referral Code:")
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .padding(AppTheme.spacing.level2),
+                        .padding(vertical = AppTheme.spacing.level2),
                     horizontalArrangement = Arrangement.Center
                 ) {
                     AppCard {
@@ -105,13 +109,14 @@ fun NewReferralDesign() {
                             )
                             AppTextButton(
                                 textToShow = "Copy",
-                                leadingIcon = Icons.Filled.ContentCopy
-                            ) { /*TODO*/ }
+                                leadingIcon = Icons.Filled.ContentCopy,
+                                onClick = copyRefCode
+                            )
                         }
                     }
                 }
                 Spacer(modifier = Modifier.height(16.dp))
-                AppDivider(modifier = Modifier.padding(horizontal = 8.dp))
+                InvitationReport()
                 Spacer(modifier = Modifier.height(16.dp))
                 Row(
                     Modifier
@@ -121,129 +126,101 @@ fun NewReferralDesign() {
                     HeadingTexts.Level2(text = "You've invited...")
                 }
                 Spacer(modifier = Modifier.height(24.dp))
-                SendReminderList()
-                SendReminderList()
-                SendReminderList()
-                SendReminderList()
-                SendReminderList()
-                SendReminderList()
-                SendReminderList()
-                JoinedUserList()
-                JoinedUserList()
-                JoinedUserList()
-                JoinedUserList()
-                ExpiredUserList()
-                ExpiredUserList()
-                ExpiredUserList()
+                InvitedUserList()
             }
         }
     }
-
 }
 
 @Composable
-fun JoinedUserList() {
-    Column {
+fun InvitedUserList(
+    userPofImg: Int = R.drawable.ic_person,
+    userName: String = "Demo Name",
+    userPhone: String = "1000000000",
+    userEmail: String = "demo@gmail.com",
+    isProMember: Boolean = true,
+    addToCommunity: () -> Unit = {}
+) {
+    AppCard(modifier = Modifier.padding(horizontal = 16.dp)) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(vertical = 16.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Column {
-                HeadingTexts.Level3(text = "Naman Yadav")
-                BodyTexts.Level3(
-                    text = "Sent on Oct 04, 2023",
-                    color = AppTheme.colors.onBackground.copy(alpha = 0.4f)
+            Row {
+                AppLocalImage(
+                    painter = painterResource(id = userPofImg),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(AppTheme.boxSize.level6)
+                        .clip(CircleShape)
                 )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    HeadingTexts.Level3(text = userName)
+                    Spacer(modifier = Modifier.height(AppTheme.spacing.level1))
+                    BodyTexts.Level3(
+                        text = userPhone.ifEmpty {
+                            userEmail
+                        },
+                        color = AppTheme.colors.onBackground.copy(alpha = 0.4f)
+                    )
+                }
             }
-            JoinedUser()
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.End) {
+                if (isProMember) {
+                    AppIcon(imageVector = Icons.Filled.Diamond, tint = AppTheme.colors.primary)
+                }
+                Spacer(modifier = Modifier.height(AppTheme.spacing.level1))
+                AddToCommunityBtn(addToCommunity = addToCommunity)
+            }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        AppDivider(modifier = Modifier.padding(horizontal = 16.dp))
     }
     Spacer(modifier = Modifier.height(16.dp))
 }
 
 @Composable
-fun SendReminderList() {
-    Column {
+fun AddToCommunityBtn(addToCommunity: () -> Unit = {}) {
+    AppIconButton(imageVector = Icons.Filled.GroupAdd, onClick = addToCommunity)
+}
+
+@Composable
+fun InvitationReport() {
+    Column(Modifier.padding(horizontal = 16.dp)) {
+        HeadingTexts.Level2(text = "Invite Report")
         Row(
-            modifier = Modifier
+            Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
+                .padding(vertical = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(AppTheme.spacing.level2),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Column {
-                HeadingTexts.Level3(text = "Naman Yadav")
-                BodyTexts.Level3(
-                    text = "Sent on Oct 04, 2023",
-                    color = AppTheme.colors.onBackground.copy(alpha = 0.4f)
-                )
-            }
-            SendReminder()
+            InvRepCard(modifier = Modifier.weight(1f))
+            InvRepCard(modifier = Modifier.weight(1f))
+            InvRepCard(modifier = Modifier.weight(1f))
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        AppDivider(modifier = Modifier.padding(horizontal = 16.dp))
     }
-    Spacer(modifier = Modifier.height(16.dp))
 }
 
 @Composable
-fun ExpiredUserList() {
-    Column {
-        Row(
+fun InvRepCard(
+    modifier: Modifier = Modifier,
+    cardTitle: String = "Demo",
+    cardValue: String = "$500"
+) {
+    AppCard(modifier = modifier) {
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Column {
-                HeadingTexts.Level3(text = "Naman")
-                BodyTexts.Level3(
-                    text = "Sent on Oct 04, 2023",
-                    color = AppTheme.colors.onBackground.copy(alpha = 0.4f)
-                )
-            }
-            ExpiredUser()
+            LargeTexts.Level3(text = cardValue)
+            Spacer(modifier = Modifier.height(AppTheme.spacing.level1))
+            CaptionTexts.Level2(text = cardTitle)
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        AppDivider(modifier = Modifier.padding(horizontal = 16.dp))
-    }
-    Spacer(modifier = Modifier.height(16.dp))
-}
-
-@Composable
-fun SendReminder() {
-    Column {
-        BodyTexts.Level3(
-            text = "Expires in 28d", color = AppTheme.colors.onBackground.copy(alpha = 0.4f)
-        )
-        CaptionTexts.Level1(text = "Send Reminder",
-            color = AppTheme.colors.primary,
-            modifier = Modifier.clickable { })
-    }
-}
-
-@Composable
-fun JoinedUser() {
-    AppTextButton(textToShow = "Add to Community", trailingIcon = Icons.Filled.GroupAdd) {
-
-    }
-}
-
-@Composable
-fun ExpiredUser() {
-    Column {
-        BodyTexts.Level2(
-            text = "Expired", color = AppTheme.colors.onBackground.copy(alpha = 0.4f)
-        )
-        CaptionTexts.Level1(
-            text = "Send Again",
-            color = AppTheme.colors.primary,
-            modifier = Modifier.clickable { })
     }
 }
