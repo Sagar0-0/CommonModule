a2d109a838acc8d0d89f51cb165bd75b211b471c
Anirban Basak <jeetbasak2002@gmail.com>
2023-11-15 19:14:07 +0530

    refactor : Google Colorful Icon set.

--- feature/auth/src/main/java/fit/asta/health/feature/auth/util/GoogleSignIn.kt
+++ feature/auth/src/main/java/fit/asta/health/feature/auth/util/GoogleSignIn.kt
@@ -4,17 +4,23 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import com.firebase.ui.auth.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.GoogleAuthProvider
+import fit.asta.health.designsystem.AppTheme
 import fit.asta.health.designsystem.molecular.button.AppOutlinedButton
+import fit.asta.health.designsystem.molecular.image.AppLocalImage
+import fit.asta.health.designsystem.molecular.texts.CaptionTexts
 import fit.asta.health.resources.strings.R as StringR
 
 @Composable
@@ -41,15 +47,30 @@ fun GoogleSignIn(
 
     AppOutlinedButton(
         modifier = modifier,
-        textToShow = stringResource(id = textId),
-        leadingIcon = painterResource(id = com.firebase.ui.auth.R.drawable.googleg_standard_color_18)
-    ) {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(token)
-            .requestEmail()
-            .build()
+        onClick = {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(token)
+                .requestEmail()
+                .build()
 
-        val googleSignInClient = GoogleSignIn.getClient(context, gso)
-        launcher.launch(googleSignInClient.signInIntent)
+            val googleSignInClient = GoogleSignIn.getClient(context, gso)
+            launcher.launch(googleSignInClient.signInIntent)
+        }
+    ) {
+
+        // Google Icon Image
+        AppLocalImage(
+            modifier = Modifier
+                .size(AppTheme.imageSize.level3)
+                .padding(end = AppTheme.spacing.level0),
+            painter = painterResource(id = R.drawable.googleg_standard_color_18),
+            contentDescription = "google icon"
+        )
+
+        // Button Text
+        CaptionTexts.Level1(
+            text = stringResource(id = textId),
+            color = AppTheme.colors.onSurface
+        )
     }
 }
