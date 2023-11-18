a84d24ad94b46c1832b5839d3c0b0d7841e13fa1
Subhash <subhashkudi59@gmail.com>
2023-11-18 21:36:00 +0530

    feat:mobile:walkingtool data package  DayRepository for local session data

---
+++ mobile/src/main/java/fit/asta/health/tools/walking/core/data/repository/DayRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package fit.asta.health.tools.walking.core.data.repository
+
+import fit.asta.health.tools.walking.core.data.source.DayDao
+import fit.asta.health.tools.walking.core.domain.model.Day
+import fit.asta.health.tools.walking.core.domain.repository.DayRepository
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+class DayRepositoryImpl(
+    private val dao: DayDao
+) : DayRepository {
+
+    override fun getTreeCount(): Flow<Int> {
+        return dao.getTreeCount()
+    }
+
+    override fun getFirstDay(): Flow<Day?> {
+        return dao.getFirstDay()
+    }
+
+    override fun getDay(date: LocalDate): Flow<Day> {
+        return dao.getDay(date)
+    }
+
+    override suspend fun getAllDays(): List<Day> {
+        return dao.getAllDays()
+    }
+
+    override fun getDays(date: LocalDate): Flow<List<Day>> {
+        return dao.getDays()
+    }
+
+    override suspend fun upsertDay(day: Day) {
+        dao.upsertDay(day)
+    }
+}
