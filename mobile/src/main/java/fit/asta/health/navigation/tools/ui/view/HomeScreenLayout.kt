a5ca692492acd76e984cbc94a280de2a01aa4906
Sagar <sagar.0dev@gmail.com>
2023-11-23 14:12:54 +0530

    home screen merge

--- mobile/src/main/java/fit/asta/health/navigation/tools/ui/view/HomeScreenLayout.kt
+++ mobile/src/main/java/fit/asta/health/navigation/tools/ui/view/HomeScreenLayout.kt
@@ -2,11 +2,15 @@ package fit.asta.health.navigation.tools.ui.view
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.items
@@ -30,7 +34,6 @@ import fit.asta.health.home.remote.model.ToolsHome
 import fit.asta.health.main.Graph
 import fit.asta.health.navigation.tools.ui.view.component.FeedbackCard
 import fit.asta.health.navigation.tools.ui.view.component.RateAppCard
-import fit.asta.health.navigation.tools.ui.view.component.ReferAndEarn
 import fit.asta.health.navigation.tools.ui.view.component.ToolsCardLayout
 import fit.asta.health.navigation.tools.ui.view.component.ToolsHmScreenTopBanner
 import fit.asta.health.navigation.tools.ui.view.component.ViewAllLayout
