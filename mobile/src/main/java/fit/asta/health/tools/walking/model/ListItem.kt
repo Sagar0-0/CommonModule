7a21e88e0ab72536b8438bea4bc4a3209bdaf0e6
Subhash <subhashkudi59@gmail.com>
2023-11-24 21:58:46 +0530

    ref:mobile: clean tools unwanted components

--- mobile/src/main/java/fit/asta/health/tools/walking/model/ListItem.kt
+++
@@ -1,6 +0,0 @@
-package fit.asta.health.tools.walking.model
-
-data class ListItem(
-    val title:String,
-    val isSelected:Boolean
-)
--- mobile/src/main/java/fit/asta/health/tools/walking/view/steps_counter/StepScreenUIEvent.kt
+++
@@ -1,8 +0,0 @@
-package fit.asta.health.tools.walking.view.steps_counter
-
-import fit.asta.health.tools.walking.view.home.StepCounterUIEvent
-
-sealed class StepScreenUIEvent {
-     object StartButtonClicked : StepScreenUIEvent()
-     object StopButtonClicked : StepScreenUIEvent()
-}
