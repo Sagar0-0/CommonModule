f2118343479118c153db1b3bf3ddf695bc7f1808
Anirban Basak <jeetbasak2002@gmail.com>
2023-12-07 05:21:58 +0530

    refactor : Code Refactor of Testimonial Create Main Screen

--- feature/testimonials/src/main/java/fit/asta/health/feature/testimonials/create/view/TestimonialForm.kt
+++ feature/testimonials/src/main/java/fit/asta/health/feature/testimonials/create/view/TestimonialForm.kt
@@ -3,32 +3,31 @@ package fit.asta.health.feature.testimonials.create.view
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import fit.asta.health.common.utils.UiState
-import fit.asta.health.common.utils.UiString
 import fit.asta.health.common.utils.toStringFromResId
 import fit.asta.health.data.testimonials.model.TestimonialType
 import fit.asta.health.designsystem.AppTheme
 import fit.asta.health.designsystem.molecular.AppInternetErrorDialog
-import fit.asta.health.designsystem.molecular.AppTextFieldValidate
 import fit.asta.health.designsystem.molecular.DialogData
 import fit.asta.health.designsystem.molecular.OnSuccessfulSubmit
 import fit.asta.health.designsystem.molecular.ShowCustomConfirmationDialog
-import fit.asta.health.designsystem.molecular.ValidateTxtLength
 import fit.asta.health.designsystem.molecular.animations.AppDotTypingAnimation
 import fit.asta.health.designsystem.molecular.background.AppScaffold
 import fit.asta.health.designsystem.molecular.background.AppTopBar
 import fit.asta.health.designsystem.molecular.button.AppFilledButton
 import fit.asta.health.designsystem.molecular.other.HandleBackPress
+import fit.asta.health.designsystem.molecular.textfield.AppOutlinedTextField
+import fit.asta.health.designsystem.molecular.textfield.AppTextFieldType
+import fit.asta.health.designsystem.molecular.textfield.AppTextFieldValidator
 import fit.asta.health.feature.testimonials.create.vm.TestimonialEvent
 import fit.asta.health.feature.testimonials.create.vm.TestimonialEvent.OnTypeChange
 import fit.asta.health.feature.testimonials.create.vm.TestimonialViewModel
@@ -47,9 +46,8 @@ fun CreateTestimonialScreen(
         topBar = {
             AppTopBar(
                 title = R.string.testimonial_title_edit.toStringFromResId(),
-                onBack = {
-                    showCustomDialogWithResult = !showCustomDialogWithResult
-                })
+                onBack = { showCustomDialogWithResult = !showCustomDialogWithResult }
+            )
         }
     ) {
         TestimonialForm(
@@ -96,8 +94,6 @@ fun TestimonialForm(
     val areInputsValid by testimonialViewModel.areInputsValid.collectAsStateWithLifecycle()
     val areMediaValid by testimonialViewModel.areMediaValid.collectAsStateWithLifecycle()
 
-    val focusRequester = remember { FocusRequester() }
-
     val radioButtonList = listOf(
         TestimonialType.TEXT,
         TestimonialType.IMAGE,
@@ -111,142 +107,143 @@ fun TestimonialForm(
     } ?: radioButtonList[0]
 
     val focusManager = LocalFocusManager.current
-    val scrollState = rememberScrollState()
 
     val event = testimonialViewModel.saveTestimonialState.collectAsState()
     val events = event.value
 
-    Box(
+    // Parent Composable Function
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(
                 top = paddingValues.calculateTopPadding(),
                 start = AppTheme.spacing.level2,
-                end = AppTheme.spacing.level2
+                end = AppTheme.spacing.level2,
+                bottom = AppTheme.spacing.level2
             )
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(AppTheme.spacing.level2)
     ) {
-        Column {
-            Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
 
-            TestimonialsRadioButton(selectionTypeText = "Testimonial Type",
-                radioButtonList = radioButtonList,
-                selectedOption = selectedOption,
-                onOptionSelected = {
-                    testimonialViewModel.onEvent(OnTypeChange(it))
-                })
+        // This function draws the testimonial radio buttons Card
+        TestimonialsRadioButtonCard(
+            cardTitle = "Testimonial Type",
+            radioButtonList = radioButtonList,
+            selectedOption = selectedOption
+        ) { testimonialViewModel.onEvent(OnTypeChange(it)) }
 
-            Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .verticalScroll(scrollState),
-                verticalArrangement = Arrangement.Center
-            ) {
-                AppTextFieldValidate(
-                    value = title.value,
-                    label = "Title",
-                    isError = title.error !is UiString.Empty,
-                    keyboardActions = KeyboardActions(onNext = {
-                        focusManager.moveFocus(FocusDirection.Down)
-                    }),
-                    onValueChange = { testimonialViewModel.onEvent(TestimonialEvent.OnTitleChange(it)) },
-                    errorMessage = title.error,
-                )
+        // Text Field to enter the title of the Testimonial
+        AppOutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = title.value,
+            label = "Title",
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            onValueChange = { testimonialViewModel.onEvent(TestimonialEvent.OnTitleChange(it)) },
+            appTextFieldType = AppTextFieldValidator(
+                AppTextFieldType.Custom(minSize = 4, maxSize = 32)
+            )
+        )
 
-                Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
+        // If text and Image testimonial is chosen then we show this text Field
+        if (selectedOption == radioButtonList[0] || selectedOption == radioButtonList[1]) {
+            AppOutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = testimonial.value,
+                label = "Testimonial",
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.clearFocus() }
+                ),
+                onValueChange = {
+                    testimonialViewModel.onEvent(TestimonialEvent.OnTestimonialChange(it))
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
+                minLines = 4,
+                maxLines = 4
+            )
+        }
 
-                if (selectedOption == radioButtonList[0] || selectedOption == radioButtonList[1]) {
-                    AppTextFieldValidate(
-                        value = testimonial.value,
-                        label = "Testimonial",
-                        keyboardActions = KeyboardActions(onNext = {
-                            focusManager.clearFocus()
-                        }),
-                        onValueChange = {
-                            testimonialViewModel.onEvent(
-                                TestimonialEvent.OnTestimonialChange(
-                                    it
-                                )
-                            )
+        // Organisation Text Field
+        AppOutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = organization.value,
+            label = "Organisation",
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            appTextFieldType = AppTextFieldValidator(
+                AppTextFieldType.Custom(minSize = 4, maxSize = 32)
+            ),
+            onValueChange = { testimonialViewModel.onEvent(TestimonialEvent.OnOrgChange(it)) }
+        )
+
+        // Role Text Field
+        AppOutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = role.value,
+            label = "Role",
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+            appTextFieldType = AppTextFieldValidator(
+                AppTextFieldType.Custom(minSize = 4, maxSize = 32)
+            ),
+            onValueChange = { testimonialViewModel.onEvent(TestimonialEvent.OnRoleChange(it)) },
+        )
+
+        // Checking which testimonial Type is selected by the user
+        when (selectedOption) {
+
+            // Text Testimonial
+            radioButtonList[0] -> {
+                // No extra UI needed
+            }
+
+            // Image Testimonial
+            radioButtonList[1] -> {
+                ImageLayout(getViewModel = testimonialViewModel)
+            }
+
+            // Video Testimonial
+            radioButtonList[2] -> {
+                TstGetVideo()
+            }
+
+            else -> {}
+        }
+
+        // Submit Button
+        AppFilledButton(
+            textToShow = "Submit",
+            modifier = Modifier.fillMaxWidth(),
+            enabled = areInputsValid && areMediaValid
+        ) {
+            showCustomDialogWithResult = !showCustomDialogWithResult
+            testimonialViewModel.onEvent(TestimonialEvent.OnSubmitTestimonial)
+        }
+
+        if (showCustomDialogWithResult) {
+            when (events) {
+
+                is UiState.Loading -> AppDotTypingAnimation()
+                is UiState.Success -> {
+                    OnSuccessfulSubmit(
+                        onDismiss = {
+                            showCustomDialogWithResult = !showCustomDialogWithResult
                         },
-                        errorMessage = testimonial.error,
-                        isError = testimonial.error !is UiString.Empty,
-                        modifier = Modifier.height(AppTheme.boxSize.level8),
-                        imeAction = if (testimonial.value.length > ValidateTxtLength.defLength) {
-                            ImeAction.Next
-                        } else {
-                            ImeAction.Default
-                        },
-                        showLenErrorMsg = true
+                        onNavigateTstHome = onNavigateTstHome,
+                        onPositiveClick = onNavigateTstHome
                     )
-                    Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
                 }
 
-                AppTextFieldValidate(
-                    value = organization.value,
-                    onValueChange = { testimonialViewModel.onEvent(TestimonialEvent.OnOrgChange(it)) },
-                    label = "Organisation",
-                    isError = organization.error !is UiString.Empty,
-                    errorMessage = organization.error,
-                    keyboardActions = KeyboardActions(onNext = {
-                        focusManager.moveFocus(FocusDirection.Down)
-                    })
-                )
-
-                Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
-
-                AppTextFieldValidate(
-                    value = role.value,
-                    onValueChange = { testimonialViewModel.onEvent(TestimonialEvent.OnRoleChange(it)) },
-                    label = "Role",
-                    isError = role.error !is UiString.Empty,
-                    errorMessage = role.error,
-                    imeAction = ImeAction.Done,
-                    modifier = Modifier.focusRequester(focusRequester = focusRequester),
-                    keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
-                )
-
-                if (selectedOption == radioButtonList[1]) {
-                    Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
-                    ImageLayout(
-                        getViewModel = testimonialViewModel,
-                    )
-                } else if (selectedOption == radioButtonList[2]) {
-                    Spacer(modifier = Modifier.height(AppTheme.spacing.level2))
-                    TstGetVideo()
-                }
-
-                AppFilledButton(
-                    textToShow = "Submit",
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = areInputsValid && areMediaValid
-                ) {
-                    showCustomDialogWithResult = !showCustomDialogWithResult
+                is UiState.NoInternet -> AppInternetErrorDialog {
                     testimonialViewModel.onEvent(TestimonialEvent.OnSubmitTestimonial)
                 }
 
-                if (showCustomDialogWithResult) {
-                    when (events) {
-
-                        is UiState.Loading -> AppDotTypingAnimation()
-                        is UiState.Success -> {
-                            OnSuccessfulSubmit(
-                                onDismiss = {
-                                    showCustomDialogWithResult = !showCustomDialogWithResult
-                                },
-                                onNavigateTstHome = onNavigateTstHome,
-                                onPositiveClick = onNavigateTstHome
-                            )
-                        }
-
-                        is UiState.NoInternet -> AppInternetErrorDialog {
-                            testimonialViewModel.onEvent(TestimonialEvent.OnSubmitTestimonial)
-                        }
-
-                        else -> {}
-                    }
-                }
+                else -> {}
             }
         }
     }
