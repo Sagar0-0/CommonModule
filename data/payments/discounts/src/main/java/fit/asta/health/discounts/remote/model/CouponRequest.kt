02fd581547ae3b3a614c37749a7a853e4972b560
Sagar <sagar.0dev@gmail.com>
2023-12-24 20:13:09 +0530

    subscription apis model changes

--- data/payments/discounts/src/main/java/fit/asta/health/discounts/remote/model/CouponRequest.kt
+++ data/payments/discounts/src/main/java/fit/asta/health/discounts/remote/model/CouponRequest.kt
@@ -1,8 +1,14 @@
 package fit.asta.health.discounts.remote.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CouponRequest(
+    @SerializedName("type")
     val productId: String = "",
+    @SerializedName("uid")
     val userId: String = "",
+    @SerializedName("couponCode")
     val couponCode: String = "",
+    @SerializedName("amt")
     val productMRP: String = "",
 )
