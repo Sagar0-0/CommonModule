e5cd6c2d72392e791424dc0a1699547dcea2df64
Sriram <sriramkalyan.p@gmail.com>
2024-01-03 08:46:19 +0530

    update: fonts integrated

--- core/designsystem/src/main/java/fit/asta/health/designsystem/atomic/token/DefaultTypographyTokens.kt
+++ core/designsystem/src/main/java/fit/asta/health/designsystem/atomic/token/DefaultTypographyTokens.kt
@@ -1,8 +1,34 @@
 package fit.asta.health.designsystem.atomic.token
 
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import fit.asta.health.core.designsystem.R
+
+
+val astaFontFamily = FontFamily(
+    Font(R.font.montserrat_black, FontWeight.Light),
+    Font(R.font.montserrat_black_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.montserrat_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.montserrat_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.montserrat_extra_light, FontWeight.ExtraLight),
+    Font(R.font.montserrat_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_semi_bold, FontWeight.SemiBold),
+    Font(R.font.montserrat_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.montserrat_thin, FontWeight.Thin),
+    Font(R.font.montserrat_thin_italic, FontWeight.Thin, FontStyle.Italic),
+)
 
 object DefaultTypographyTokens {
 
@@ -11,15 +37,18 @@ object DefaultTypographyTokens {
      */
     val largeLevel1: TextStyle = TextStyle(
         fontSize = 72.sp,
-        fontWeight = FontWeight.Normal
+        fontWeight = FontWeight.Normal,
+        fontFamily = astaFontFamily
     )
     val largeLevel2: TextStyle = TextStyle(
         fontSize = 48.sp,
-        fontWeight = FontWeight.SemiBold
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = astaFontFamily
     )
     val largeLevel3: TextStyle = TextStyle(
         fontSize = 32.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = FontWeight.Bold,
+        fontFamily = astaFontFamily
     )
 
     /**
@@ -27,19 +56,23 @@ object DefaultTypographyTokens {
      */
     val headingLevel1: TextStyle = TextStyle(
         fontSize = 24.sp,
-        fontWeight = FontWeight.SemiBold
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = astaFontFamily
     )
     val headingLevel2: TextStyle = TextStyle(
         fontSize = 24.sp,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontFamily = astaFontFamily
     )
     val headingLevel3: TextStyle = TextStyle(
         fontSize = 20.sp,
-        fontWeight = FontWeight.SemiBold
+        fontWeight = FontWeight.SemiBold,
+        fontFamily = astaFontFamily
     )
     val headingLevel4: TextStyle = TextStyle(
         fontSize = 20.sp,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontFamily = astaFontFamily
     )
 
     /**
@@ -47,19 +80,23 @@ object DefaultTypographyTokens {
      */
     val titleLevel1: TextStyle = TextStyle(
         fontSize = 20.sp,
-        fontWeight = FontWeight.Normal
+        fontWeight = FontWeight.Normal,
+        fontFamily = astaFontFamily
     )
     val titleLevel2: TextStyle = TextStyle(
         fontSize = 18.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = FontWeight.Bold,
+        fontFamily = astaFontFamily
     )
     val titleLevel3: TextStyle = TextStyle(
         fontSize = 18.sp,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontFamily = astaFontFamily
     )
     val titleLevel4: TextStyle = TextStyle(
         fontSize = 16.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = FontWeight.Bold,
+        fontFamily = astaFontFamily
     )
 
     /**
@@ -67,15 +104,18 @@ object DefaultTypographyTokens {
      */
     val bodyLevel1: TextStyle = TextStyle(
         fontSize = 16.sp,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontFamily = astaFontFamily
     )
     val bodyLevel2: TextStyle = TextStyle(
         fontSize = 16.sp,
-        fontWeight = FontWeight.Normal
+        fontWeight = FontWeight.Normal,
+        fontFamily = astaFontFamily
     )
     val bodyLevel3: TextStyle = TextStyle(
         fontSize = 14.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = FontWeight.Bold,
+        fontFamily = astaFontFamily
     )
 
     /**
@@ -83,23 +123,27 @@ object DefaultTypographyTokens {
      */
     val captionLevel1: TextStyle = TextStyle(
         fontSize = 14.sp,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontFamily = astaFontFamily
     )
     val captionLevel2: TextStyle = TextStyle(
         fontSize = 14.sp,
-        fontWeight = FontWeight.Normal
+        fontWeight = FontWeight.Normal,
+        fontFamily = astaFontFamily
     )
     val captionLevel3: TextStyle = TextStyle(
         fontSize = 12.sp,
-        fontWeight = FontWeight.Medium
+        fontWeight = FontWeight.Medium,
+        fontFamily = astaFontFamily
     )
     val captionLevel4: TextStyle = TextStyle(
         fontSize = 12.sp,
-        fontWeight = FontWeight.Normal
+        fontWeight = FontWeight.Normal,
+        fontFamily = astaFontFamily
     )
     val captionLevel5: TextStyle = TextStyle(
         fontSize = 10.sp,
-        fontWeight = FontWeight.Bold
+        fontWeight = FontWeight.Bold,
+        fontFamily = astaFontFamily
     )
-
 }
