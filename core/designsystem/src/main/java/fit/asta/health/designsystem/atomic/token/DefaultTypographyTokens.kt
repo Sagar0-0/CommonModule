57b04728b1dc2277dbca49f80e8737b38370f4ff
Sriram <sriramkalyan.p@gmail.com>
2024-01-04 21:45:37 +0530

    refactor: fonts adapter

--- core/designsystem/src/main/java/fit/asta/health/designsystem/atomic/token/DefaultTypographyTokens.kt
+++ core/designsystem/src/main/java/fit/asta/health/designsystem/atomic/token/DefaultTypographyTokens.kt
@@ -1,34 +1,8 @@
 package fit.asta.health.designsystem.atomic.token
 
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import fit.asta.health.core.designsystem.R
-
-
-val astaFontFamily = FontFamily(
-    Font(R.font.montserrat_black, FontWeight.Light),
-    Font(R.font.montserrat_black_italic, FontWeight.Normal, FontStyle.Italic),
-    Font(R.font.montserrat_bold, FontWeight.Bold),
-    Font(R.font.montserrat_bold_italic, FontWeight.Bold, FontStyle.Italic),
-    Font(R.font.montserrat_extra_bold, FontWeight.ExtraBold),
-    Font(R.font.montserrat_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
-    Font(R.font.montserrat_extra_light, FontWeight.ExtraLight),
-    Font(R.font.montserrat_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
-    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
-    Font(R.font.montserrat_light, FontWeight.Light),
-    Font(R.font.montserrat_light_italic, FontWeight.Light, FontStyle.Italic),
-    Font(R.font.montserrat_medium, FontWeight.Medium),
-    Font(R.font.montserrat_medium_italic, FontWeight.Medium, FontStyle.Italic),
-    Font(R.font.montserrat_regular, FontWeight.Normal),
-    Font(R.font.montserrat_semi_bold, FontWeight.SemiBold),
-    Font(R.font.montserrat_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
-    Font(R.font.montserrat_thin, FontWeight.Thin),
-    Font(R.font.montserrat_thin_italic, FontWeight.Thin, FontStyle.Italic),
-)
 
 object DefaultTypographyTokens {
 
---
+++ core/designsystem/src/main/java/fit/asta/health/designsystem/atomic/token/FontsAdapter.kt
@@ -0,0 +1,28 @@
+package fit.asta.health.designsystem.atomic.token
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import fit.asta.health.core.designsystem.R.font
+
+val astaFontFamily = FontFamily(
+    Font(font.montserrat_black, FontWeight.Light),
+    Font(font.montserrat_black_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(font.montserrat_bold, FontWeight.Bold),
+    Font(font.montserrat_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(font.montserrat_extra_bold, FontWeight.ExtraBold),
+    Font(font.montserrat_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(font.montserrat_extra_light, FontWeight.ExtraLight),
+    Font(font.montserrat_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(font.montserrat_light, FontWeight.Light),
+    Font(font.montserrat_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(font.montserrat_medium, FontWeight.Medium),
+    Font(font.montserrat_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(font.montserrat_regular, FontWeight.Normal),
+    Font(font.montserrat_semi_bold, FontWeight.SemiBold),
+    Font(font.montserrat_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(font.montserrat_thin, FontWeight.Thin),
+    Font(font.montserrat_thin_italic, FontWeight.Thin, FontStyle.Italic),
+)
