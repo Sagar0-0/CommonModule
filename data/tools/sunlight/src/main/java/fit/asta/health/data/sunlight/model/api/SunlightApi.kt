ca99507a8713592523b1ccdb2e4bbaa1875dbf4b
Sagar <sagar.0dev@gmail.com>
2024-02-04 12:16:02 +0530

    [Fix]: Sunlight tool crash fix

--- data/tools/sunlight/src/main/java/fit/asta/health/data/sunlight/model/api/SunlightApi.kt
+++ data/tools/sunlight/src/main/java/fit/asta/health/data/sunlight/model/api/SunlightApi.kt
@@ -1,15 +1,19 @@
 package fit.asta.health.data.sunlight.model.api
 
-import fit.asta.health.data.sunlight.model.network.response.ResponseData
+import fit.asta.health.common.utils.Response
+import fit.asta.health.data.sunlight.model.network.response.SunlightToolData
+import retrofit2.http.*
+
 
 //Health Tool - Sunlight Endpoints
 interface SunlightApi {
-    suspend fun getSunlightTool(
-        userId: String,
-        latitude: String,
-        longitude: String,
-        date: String,
-        location: String
-    ): ResponseData
 
+    @GET("tools/sunlight/get?")
+    suspend fun getSunlightTool(
+        @Query("userId") userId: String = "6309a9379af54f142c65fbfe",
+        @Query("lat") latitude: String = "",
+        @Query("lon") longitude: String = "",
+        @Query("date") date: String = "",
+        @Query("loc") location: String = ""
+    ): Response<SunlightToolData>
 }
